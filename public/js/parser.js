7c971abdadb6aceb304ea792a39202e87e9225cf
LandonPowell <landonjpowell@airmail.cc>
2016-07-14 00:38:19 +0000

    Changed <br>

--- public/js/parser.js
+++ public/js/parser.js
@@ -8,7 +8,7 @@ var parser = {
                      .replace(/\"/g, "&quot;"   )
                      .replace(/\\/g, "&bsol;"   )
                      .replace(/  /g, " &nbsp;"  )
-                     .replace(/\n/g, " <br> "   )
+                     .replace(/\n/g, " <br>\n"  )
                      .replace(/\t/g, " &nbsp; &nbsp;");
     },
     quote : function(string) { /* THIS CREATES THE QUOTES/GREENTEXT */
