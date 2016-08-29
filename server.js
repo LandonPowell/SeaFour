bbb87ecbf77e24bfc31ef7f1aa144552e2d0befd
LandonPowell <landonjpowell@airmail.cc>
2016-08-29 12:35:37 +0000

    Line reduction. Empty lines carried no meaning and weren't good whitespace.

--- server.js
+++ server.js
@@ -372,7 +372,6 @@ io.on('connection', function(socket) {
                 "flair"     : null,
                 "corp"      : 0, /* Becomes an object upon incorporation */
                 "role"      : 0, /* Default role is 0 */
-                
             };
             updateDatabase(socket, "You are now registered.");
 
@@ -391,7 +390,6 @@ io.on('connection', function(socket) {
                 "flair"     : null,
                 "corp"      : 0, /* Becomes an object upon incorporation */
                 "role"      : 0, /* Default role is 0 */
-                
             };
             updateDatabase(socket, "You are now registered.");
         }
