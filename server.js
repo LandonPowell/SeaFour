dfefc28328c05bea6b6d76f3bc4e9d47677d5e9c
LandonPowell <landonjpowell@airmail.cc>
2016-09-05 01:36:26 +0000

    Didn't need this.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
-var pug     = require('pug');
 
 var hash        = require('./lib/hash');
 var jsonfile    = require('jsonfile');
